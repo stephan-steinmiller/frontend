78b9aa46af77dea08806283c7c693074ed3a2093
Lauritz Tieste <84938977+Lauritz-Tieste@users.noreply.github.com>
2024-05-02 21:52:31 +0200

    Multiple Improvements in the UI (#95)
    * Updated quiz description depending on single/multiple choice
    
    * Update German translation for empty quiz message
    
    * Update calendar buttons text
    
    * Updated submit button label for multiple choice quiz questions
    
    * Add conditional rendering for no upcoming events
    
    * Update button margin and width in Profile.vue
    
    * Update height for Coins and Hearts components on NavbarMenu
    
    * fixed typo of Bootstrap-Academy/#86
    
    * Fixed unused String in MFA-Initialisation
    
    * fix build errors with missing comma
    
    * Remove unnecessary duplication

--- pages/account/mfa/initialize.vue
+++ pages/account/mfa/initialize.vue
@@ -18,40 +18,40 @@
 -->
 
 <template>
-	<section
-		class="container-fluid pt-container pb-container h-screen-inner min grid place-items-center"
-	>
-		<Transition mode="out-in" name="slide-up-down">
-			<Dialog v-if="dialog && dialog.type" :dialog="dialog">
-				<template #content>
-					<div
-						v-if="!!TOTP_secret"
-						class="text-body-1 text-body font-body m-0 mt-box"
-					>
-						{{ t('Headings.TOTPSecret') }}:
-						<span class="allow-selection">{{ TOTP_secret }}</span>
-						<div class="bg-white p-10 w-fit h-fit mt-card">
-							<qrcode-vue :size="200" :value="QR_code"></qrcode-vue>
-						</div>
-					</div>
-				</template>
-			</Dialog>
-		</Transition>
-	</section>
+  <section
+    class="container-fluid pt-container pb-container h-screen-inner min grid place-items-center"
+  >
+    <Transition mode="out-in" name="slide-up-down">
+      <Dialog v-if="dialog && dialog.type" :dialog="dialog">
+        <template #content>
+          <div
+            v-if="!!TOTP_secret"
+            class="text-body-1 text-body font-body m-0 mt-box"
+          >
+            {{ t("Headings.TOTPSecret") }}:
+            <span class="allow-selection">{{ TOTP_secret }}</span>
+            <div class="bg-white p-10 w-fit h-fit mt-card">
+              <qrcode-vue :size="200" :value="QR_code"></qrcode-vue>
+            </div>
+          </div>
+        </template>
+      </Dialog>
+    </Transition>
+  </section>
 </template>
 
 <script lang="ts">
-import { useI18n } from 'vue-i18n';
-import QrcodeVue from 'qrcode.vue';
+import { useI18n } from "vue-i18n";
+import QrcodeVue from "qrcode.vue";
 
 definePageMeta({
-  layout: 'inner',
-  middleware: ['auth'],
+  layout: "inner",
+  middleware: ["auth"],
 });
 
 export default {
   head: {
-    title: 'Initialize MFA',
+    title: "Initialize MFA",
   },
   components: { QrcodeVue },
   setup() {
@@ -61,17 +61,17 @@ export default {
     const user = <any>useUser();
 
     // ============================================================= OR Code
-    const QR_code = ref('');
+    const QR_code = ref("");
     function getQR_code(secret: any, username: any) {
       QR_code.value = `otpauth://totp/${encodeURIComponent(
         username
       )}?secret=${encodeURIComponent(secret)}&issuer=${encodeURIComponent(
-        'Bootstrap Academy'
+        "Bootstrap Academy"
       )}`;
     }
 
     const dialog = <any>reactive({});
-    const TOTP_secret = ref('');
+    const TOTP_secret = ref("");
 
     onMounted(async () => {
       setLoading(true);
@@ -84,15 +84,15 @@ export default {
       }
 
       Object.assign(dialog, {
-        type: !!success ? 'success' : 'error',
-        heading: 'InitializeMFA',
+        type: !!success ? "success" : "error",
+        heading: "InitializeMFA",
         body: !!success
-          ? 'Success.InitializeMFA'
-          : `${t('Error.InitializeMFA')}: ${error?.detail ?? ''}`,
+          ? "Success.InitializeMFA"
+          : `${t("Error.InitializeMFA")}: ${error?.detail ?? ""}`,
         primaryBtn: {
-          label: !!success ? 'Buttons.EnableMFA' : 'Links.GoBack',
+          label: !!success ? "Buttons.EnableMFA" : "Links.GoBack",
           onclick: () => {
-            router.push(!!success ? '/account/mfa/enable' : '/account');
+            router.push(!!success ? "/account/mfa/enable" : "/account");
           },
         },
         secondaryBtn: null,
